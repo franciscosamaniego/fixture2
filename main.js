28191a8d5b5029f04f9d069a5f2976439d74ef1e
franciscosamaniego <franciscosamaniego.fs@gmail.com>
2022-06-02 12:31:11 -0300

    fixture

---
+++ main.js
@@ -0,0 +1,216 @@
+document.getElementById('tabla_A').style.display = 'none';
+document.getElementById('fecha_A').style.display = 'none';
+document.getElementById('tabla_B').style.display = 'none';
+document.getElementById('fecha_B').style.display = 'none';
+document.getElementById('tabla_C').style.display = 'none';
+document.getElementById('fecha_C').style.display = 'none';
+document.getElementById('tabla_D').style.display = 'none';
+document.getElementById('fecha_D').style.display = 'none';
+document.getElementById('tabla_E').style.display = 'none';
+document.getElementById('fecha_E').style.display = 'none';
+document.getElementById('tabla_F').style.display = 'none';
+document.getElementById('fecha_F').style.display = 'none';
+document.getElementById('tabla_G').style.display = 'none';
+document.getElementById('fecha_G').style.display = 'none';
+document.getElementById('tabla_H').style.display = 'none';
+document.getElementById('fecha_H').style.display = 'none';
+
+function Grupo_A() {
+  document.getElementById('tabla_A').style.display = 'block';
+  document.getElementById('fecha_A').style.display = 'block';
+  document.getElementById('tabla_B').style.display = 'none';
+  document.getElementById('fecha_B').style.display = 'none';
+  document.getElementById('tabla_C').style.display = 'none';
+  document.getElementById('fecha_C').style.display = 'none';
+  document.getElementById('tabla_D').style.display = 'none';
+  document.getElementById('fecha_D').style.display = 'none';
+  document.getElementById('tabla_E').style.display = 'none';
+  document.getElementById('fecha_E').style.display = 'none';
+  document.getElementById('tabla_F').style.display = 'none';
+  document.getElementById('fecha_F').style.display = 'none';
+  document.getElementById('tabla_G').style.display = 'none';
+  document.getElementById('fecha_G').style.display = 'none';
+  document.getElementById('tabla_H').style.display = 'none';
+  document.getElementById('fecha_H').style.display = 'none';
+}
+function Grupo_B() {
+  document.getElementById('tabla_B').style.display = 'block';
+  document.getElementById('fecha_B').style.display = 'block';
+  document.getElementById('tabla_A').style.display = 'none';
+  document.getElementById('fecha_A').style.display = 'none';
+  document.getElementById('tabla_C').style.display = 'none';
+  document.getElementById('fecha_C').style.display = 'none';
+  document.getElementById('tabla_D').style.display = 'none';
+  document.getElementById('fecha_D').style.display = 'none';
+  document.getElementById('tabla_E').style.display = 'none';
+  document.getElementById('fecha_E').style.display = 'none';
+  document.getElementById('tabla_F').style.display = 'none';
+  document.getElementById('fecha_F').style.display = 'none';
+  document.getElementById('tabla_G').style.display = 'none';
+  document.getElementById('fecha_G').style.display = 'none';
+  document.getElementById('tabla_H').style.display = 'none';
+  document.getElementById('fecha_H').style.display = 'none';
+}
+function Grupo_C() {
+  document.getElementById('tabla_C').style.display = 'block';
+  document.getElementById('fecha_C').style.display = 'block';
+  document.getElementById('tabla_A').style.display = 'none';
+  document.getElementById('fecha_A').style.display = 'none';
+  document.getElementById('tabla_B').style.display = 'none';
+  document.getElementById('fecha_B').style.display = 'none';
+  document.getElementById('tabla_D').style.display = 'none';
+  document.getElementById('fecha_D').style.display = 'none';
+  document.getElementById('tabla_E').style.display = 'none';
+  document.getElementById('fecha_E').style.display = 'none';
+  document.getElementById('tabla_F').style.display = 'none';
+  document.getElementById('fecha_F').style.display = 'none';
+  document.getElementById('tabla_G').style.display = 'none';
+  document.getElementById('fecha_G').style.display = 'none';
+  document.getElementById('tabla_H').style.display = 'none';
+  document.getElementById('fecha_H').style.display = 'none';
+}
+function Grupo_D() {
+  document.getElementById('tabla_D').style.display = 'block';
+  document.getElementById('fecha_D').style.display = 'block';
+  document.getElementById('tabla_A').style.display = 'none';
+  document.getElementById('fecha_A').style.display = 'none';
+  document.getElementById('tabla_B').style.display = 'none';
+  document.getElementById('fecha_B').style.display = 'none';
+  document.getElementById('tabla_C').style.display = 'none';
+  document.getElementById('fecha_C').style.display = 'none';
+  document.getElementById('tabla_E').style.display = 'none';
+  document.getElementById('fecha_E').style.display = 'none';
+  document.getElementById('tabla_F').style.display = 'none';
+  document.getElementById('fecha_F').style.display = 'none';
+  document.getElementById('tabla_G').style.display = 'none';
+  document.getElementById('fecha_G').style.display = 'none';
+  document.getElementById('tabla_H').style.display = 'none';
+  document.getElementById('fecha_H').style.display = 'none';
+}
+function Grupo_E() {
+  document.getElementById('tabla_E').style.display = 'block';
+  document.getElementById('fecha_E').style.display = 'block';
+  document.getElementById('tabla_A').style.display = 'none';
+  document.getElementById('fecha_A').style.display = 'none';
+  document.getElementById('tabla_B').style.display = 'none';
+  document.getElementById('fecha_B').style.display = 'none';
+  document.getElementById('tabla_C').style.display = 'none';
+  document.getElementById('fecha_C').style.display = 'none';
+  document.getElementById('tabla_D').style.display = 'none';
+  document.getElementById('fecha_D').style.display = 'none';
+  document.getElementById('tabla_F').style.display = 'none';
+  document.getElementById('fecha_F').style.display = 'none';
+  document.getElementById('tabla_G').style.display = 'none';
+  document.getElementById('fecha_G').style.display = 'none';
+  document.getElementById('tabla_H').style.display = 'none';
+  document.getElementById('fecha_H').style.display = 'none';
+}
+function Grupo_F() {
+  document.getElementById('tabla_F').style.display = 'block';
+  document.getElementById('fecha_F').style.display = 'block';
+  document.getElementById('tabla_A').style.display = 'none';
+  document.getElementById('fecha_A').style.display = 'none';
+  document.getElementById('tabla_B').style.display = 'none';
+  document.getElementById('fecha_B').style.display = 'none';
+  document.getElementById('tabla_C').style.display = 'none';
+  document.getElementById('fecha_C').style.display = 'none';
+  document.getElementById('tabla_D').style.display = 'none';
+  document.getElementById('fecha_D').style.display = 'none';
+  document.getElementById('tabla_E').style.display = 'none';
+  document.getElementById('fecha_E').style.display = 'none';
+  document.getElementById('tabla_G').style.display = 'none';
+  document.getElementById('fecha_G').style.display = 'none';
+  document.getElementById('tabla_H').style.display = 'none';
+  document.getElementById('fecha_H').style.display = 'none';
+}
+function Grupo_G() {
+  document.getElementById('tabla_G').style.display = 'block';
+  document.getElementById('fecha_G').style.display = 'block';
+  document.getElementById('tabla_A').style.display = 'none';
+  document.getElementById('fecha_A').style.display = 'none';
+  document.getElementById('tabla_B').style.display = 'none';
+  document.getElementById('fecha_B').style.display = 'none';
+  document.getElementById('tabla_C').style.display = 'none';
+  document.getElementById('fecha_C').style.display = 'none';
+  document.getElementById('tabla_D').style.display = 'none';
+  document.getElementById('fecha_D').style.display = 'none';
+  document.getElementById('tabla_E').style.display = 'none';
+  document.getElementById('fecha_E').style.display = 'none';
+  document.getElementById('tabla_F').style.display = 'none';
+  document.getElementById('fecha_F').style.display = 'none';
+  document.getElementById('tabla_H').style.display = 'none';
+  document.getElementById('fecha_H').style.display = 'none';
+}
+function Grupo_H() {
+  document.getElementById('tabla_H').style.display = 'block';
+  document.getElementById('fecha_H').style.display = 'block';
+  document.getElementById('tabla_A').style.display = 'none';
+  document.getElementById('fecha_A').style.display = 'none';
+  document.getElementById('tabla_B').style.display = 'none';
+  document.getElementById('fecha_B').style.display = 'none';
+  document.getElementById('tabla_C').style.display = 'none';
+  document.getElementById('fecha_C').style.display = 'none';
+  document.getElementById('tabla_D').style.display = 'none';
+  document.getElementById('fecha_D').style.display = 'none';
+  document.getElementById('tabla_E').style.display = 'none';
+  document.getElementById('fecha_E').style.display = 'none';
+  document.getElementById('tabla_F').style.display = 'none';
+  document.getElementById('fecha_F').style.display = 'none';
+  document.getElementById('tabla_G').style.display = 'none';
+  document.getElementById('fecha_G').style.display = 'none';
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let local = event.target.children[0].textContent;
+  let visitante = event.target.children[3].textContent;
+  let golesLocal = event.target.children[1].value;
+  let golesVisitante = event.target.children[2].value;
+
+  let ganoLocal = false;
+  let empate = false;
+  if (golesLocal > golesVisitante) ganoLocal = true;
+  else if (golesLocal == golesVisitante) empate = true;
+
+
+  // console.log(`${local} ${golesLocal} | ${golesVisitante} ${visitante}`);
+  let tds = document.querySelectorAll('td');
+  for (let td of tds) {
+    if (td.textContent == local || td.textContent == visitante) {
+      // si empatan
+      if (empate) {
+        td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent = parseInt(td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent) + 1;
+        td.nextElementSibling.textContent = parseInt(td.nextElementSibling.textContent) + 1;
+      }
+      // PJ + 1
+      td.nextElementSibling.nextElementSibling.textContent = parseInt(td.nextElementSibling.nextElementSibling.textContent) + 1;
+    }
+    if (td.textContent == local) {
+      // GF del local
+      td.parentElement.children[7].textContent = parseInt(td.parentElement.children[7].textContent) + parseInt(golesLocal);
+      // GC del local
+      td.parentElement.children[8].textContent = parseInt(td.parentElement.children[8].textContent) + parseInt(golesVisitante);
+      if (golesLocal > golesVisitante) {
+        td.nextElementSibling.nextElementSibling.nextElementSibling.textContent = parseInt(td.nextElementSibling.nextElementSibling.nextElementSibling.textContent) + 1;
+        td.nextElementSibling.textContent = (parseInt(td.parentElement.children[4].textContent) * 3) + parseInt(td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent);
+      }
+      if (golesLocal < golesVisitante) {
+        td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent = parseInt(td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent) + 1;
+      }
+    }
+    if (td.textContent == visitante) {
+      // GF del visitante
+      td.parentElement.children[7].textContent = parseInt(td.parentElement.children[7].textContent) + parseInt(golesVisitante);
+      // GC del visitante
+      td.parentElement.children[8].textContent = parseInt(td.parentElement.children[8].textContent) + parseInt(golesLocal);
+      if (golesVisitante > golesLocal) {
+        td.nextElementSibling.nextElementSibling.nextElementSibling.textContent = parseInt(td.nextElementSibling.nextElementSibling.nextElementSibling.textContent) + 1;
+        td.nextElementSibling.textContent = (parseInt(td.parentElement.children[4].textContent) * 3) + parseInt(td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent);
+      }
+      if (golesVisitante < golesLocal) {
+        td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent = parseInt(td.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling.textContent) + 1;
+      }
+    }
+    td.parentElement.children[9].textContent = parseInt(td.parentElement.children[7].textContent) - parseInt(td.parentElement.children[8].textContent);
+  }
+}
